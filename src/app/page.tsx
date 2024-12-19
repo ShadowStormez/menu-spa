b5f04cec2f7cdfc5185f2d00fd9f1edd9ad002fa
aria tn <ariatn8204@gmail.com>
2024-12-19 16:51:20 +0330

    fix:fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,23 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react'; // Import Suspense
-import React, { useEffect } from 'react';
+import React from 'react';
 import Hero from "./components/Hero/Hero";
 import HomeCard from "./components/HomeCard/HomeCard";
 import { HomePageStyle } from "./page.Style";
 import { menu, AI } from "@/app/assets/icons";
-import { useDispatch } from 'react-redux';
-import { setRestaurantId, setTableId } from './store/globalSlice';
-import { useSelector } from 'react-redux';
-import { RootState } from '../app/store'; 
 
 //mui
 import { Typography, LinearProgress } from '@mui/material';
 
 // utils
 import useRestaurantProfile from '@/app/utils/useRestaurantProfile';
+import useInitializeParams from '@/app/utils/useInitializeParams';
 
-function HomeSearch() {
-  const searchParams = useSearchParams();
-  const dispatch=useDispatch()
-  
-  useEffect(() => {
-    const restaurantId = searchParams.get('restaurantId');
-    const tableId = searchParams.get('tableId');
 
-    if (restaurantId) {
-      dispatch(setRestaurantId(restaurantId));
-    }
-
-    if (tableId) {
-      dispatch(setTableId(Number(tableId)));
-    }
-  }, [searchParams, dispatch]);
-
-  return null;
-}
 
 export default function Home() {
-  
-  const restaurantId = useSelector((state: RootState) => state.global.restaurantId);
-  const { restaurantData,loading,error } = useRestaurantProfile(restaurantId);
+  useInitializeParams();
+  const { restaurantData,loading,error } = useRestaurantProfile();
 
   if (loading) {
     return <LinearProgress />;
@@ -57,7 +33,6 @@ export default function Home() {
 
   return (
       <>
-      <HomeSearch />
       <HomePageStyle>
         <Hero restaurantName={restaurantData?.data.name} />
         <div className="HomeCards-Container">
--- src/app/utils/useRestaurantProfile.ts
+++ src/app/utils/useRestaurantProfile.ts
@@ -1,33 +1,37 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { RestaurantProfile } from '../types/restaurant'; // Global type for restaurant
-import { useDispatch } from 'react-redux';
-import { setRestaurantDetails } from '../store/globalSlice'; // Import the action
+import { RestaurantProfile } from '../types/restaurant';
+import { useDispatch, useSelector } from 'react-redux';
+import { setRestaurantDetails } from '../store/globalSlice';
+import { RootState } from '../store';
 
-export default function useRestaurantProfile(restaurantId: string | null) {
-  const [restaurantData, setRestaurantData] = useState<RestaurantProfile>();
-  const dispatch = useDispatch();
-
+export default function useRestaurantProfile() {
+  const [restaurantData, setRestaurantData] = useState<RestaurantProfile | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
+  const restaurantId = useSelector((state: RootState) => state.global.restaurantId);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     if (!restaurantId) {
+      setError('Restaurant ID is missing');
       setLoading(false);
       return;
     }
 
     const fetchRestaurantProfile = async () => {
       try {
+        setLoading(true);
         const response = await axios.get(`http://menyou-svc-gw.darkube.app/api/v1/restaurants/${restaurantId}/profile`);
         setRestaurantData(response.data);
-        setError(null);
         dispatch(setRestaurantDetails({
           name: response.data?.data.name,
           address: response.data?.data.address,
         }));
+        setError(null);
       } catch (err) {
-        setError("Failed to load restaurant data");
+        setError('Failed to load restaurant data');
       } finally {
         setLoading(false);
       }
@@ -38,4 +42,3 @@ export default function useRestaurantProfile(restaurantId: string | null) {
 
   return { restaurantData, loading, error };
 }
-
