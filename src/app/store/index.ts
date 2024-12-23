a57cc697c79b0f5088b3366493f41728a73f58e8
aria tn <ariatn8204@gmail.com>
2024-12-23 21:54:01 +0330

    temp

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,18 +1,34 @@
-// store/index.js
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import categoryReducer from './categorySlice';
 import cartReducer from './cartSlice';
-import globalReducer from './globalSlice'
-import authReducer from './authSlice'
+import globalReducer from './globalSlice';
+import authReducer from './authSlice';
 
-const store = configureStore({
-  reducer: {
+// Persist Configurations
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['global'], // Specify reducers to persist
+};
+
+// Wrapping specific reducers with persistReducer
+const rootReducer = {
     category: categoryReducer,
     cart: cartReducer,
-    global: globalReducer,
+    global: persistReducer(persistConfig, globalReducer),
     auth: authReducer,
-  },
+};
+
+// Configure Store with Redux DevTools Enabled
+const store = configureStore({
+    reducer: rootReducer,
+    devTools: process.env.NODE_ENV !== 'production', // Enable Redux DevTools in development mode
 });
+
+// Create Persistor
+export const persistor = persistStore(store);
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
