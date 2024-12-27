0863a5c9b965cd44abd299cb605d3544426430a9
aria tn <ariatn8204@gmail.com>
2024-12-27 20:07:47 +0330

    fix:let's gooooo

--- src/app/Menu/page.Style.ts
+++ src/app/Menu/page.Style.ts
@@ -1,5 +1,14 @@
 import styled from "@emotion/styled"
 export const MenuPageStyle=styled.div`
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1001; /* Place behind the sidebar */
+  }
 
 .hero-tablist-container {
   position: relative;
