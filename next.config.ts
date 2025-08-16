9aca836a3cf9fd6f68d8ae6b157eeec4b404fa07
Aria Tehrani <jarvis@JARVIS.local>
2025-08-16 09:50:03 +0330

    standalone added

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     domains: ['menyou-svc-gw.darkube.app'],
   },
 };
 
 export default nextConfig;
+
 
