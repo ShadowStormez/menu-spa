ab1c2c07fd0c7b546a231bc77fc3d08b38a5f44a
Aria Tehrani <jarvis@JARVIS.local>
2025-08-19 02:54:45 +0330

    backend integration test

--- src/app/utils/useRestaurantProfile.ts
+++ src/app/utils/useRestaurantProfile.ts
@@ -14,7 +14,7 @@ export default function useRestaurantProfile(restaurantId:string | null) {
     const fetchRestaurantProfile = async () => {
       try {
         console.log("Using restaurantId:", restaurantId);
-        const response = await axios.get(`https://menyou-svc-gw.darkube.app/api/v1/restaurants/${restaurantId}/profile`);
+        const response = await axios.get(`http://menyou-svc-gw.darkube.app/api/v1/restaurants/${restaurantId}/profile`);
         
         setRestaurantData(response.data);
 
